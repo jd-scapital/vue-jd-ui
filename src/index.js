be7734642400640d9d8f9acb7faae1a5c217fd80
siqing <siqing@scapital.com>
2019-05-16 17:53:30 +0800

    [build] 1.0.0-dev.14

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '1.0.0-dev.13',
+  version: '1.0.0-dev.14',
   install,
   CollapseTransition,
   Loading: ElLoading,
