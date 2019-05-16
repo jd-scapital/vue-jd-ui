70b05cf8faaa24cc33986f29d7b6e90e9fb50282
siqing <siqing@scapital.com>
2019-05-16 11:11:42 +0800

    修改分页的语言提示

--- packages/el-pagination/index.js
+++ packages/el-pagination/index.js
@@ -1,7 +1,11 @@
 import ElPagination from './src/pagination'
+import Locale from 'vue-jd-ui/src/mixins/locale'
+import zhLocale from 'vue-jd-ui/src/locale/lang/zh-CN'
+zhLocale.el.pagination.goto = '跳至 '
 
 /* istanbul ignore next */
 ElPagination.install = function(Vue) {
+  Locale.use(zhLocale)
   Vue.component(ElPagination.name, ElPagination)
 }
 
