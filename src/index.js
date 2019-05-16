12c3ac8f676eb761d81f48ea00e47a1aa9fac5d3
liyanfeng <yanfeng@scapital.com>
2019-05-16 10:18:53 +0800

    [build] 1.0.0-dev.10

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '1.0.0-dev.9',
+  version: '1.0.0-dev.10',
   install,
   CollapseTransition,
   Loading: ElLoading,
