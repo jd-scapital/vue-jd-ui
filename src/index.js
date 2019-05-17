0b2e6a3514b7f35659067f0418345e765d18698d
siqing <siqing@scapital.com>
2019-05-17 14:05:51 +0800

    [build] 1.0.0-dev.18

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '1.0.0-dev.17',
+  version: '1.0.0-dev.18',
   install,
   CollapseTransition,
   Loading: ElLoading,
