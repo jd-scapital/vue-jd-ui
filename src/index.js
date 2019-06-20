bdb3dcffc63b56e6fce3d61b154eaa09e0b0722e
liyanfeng <yanfeng@scapital.com>
2019-06-20 10:38:09 +0800

    增加gitignore

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '1.0.0-beta.13',
+  version: '1.0.0-beta.14',
   install,
   CollapseTransition,
   Loading: ElLoading,
