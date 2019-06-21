6cbe16949feaa43f83b7dd5bb46fa3cb54930420
liyanfeng <yanfeng@scapital.com>
2019-06-21 16:19:11 +0800

    [build] 1.0.0-beta.19

--- src/index.js
+++ src/index.js
@@ -88,7 +88,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '1.0.0-beta.18',
+  version: '1.0.0-beta.19',
   install,
   CollapseTransition,
   Loading: ElLoading,
