df66f7d2327db50b538bee6e982ed59434df9c3a
siqing <siqing@scapital.com>
2019-06-27 12:02:31 +0800

    [build] 1.0.0-beta.31

--- src/index.js
+++ src/index.js
@@ -88,7 +88,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '1.0.0-beta.30',
+  version: '1.0.0-beta.31',
   install,
   CollapseTransition,
   Loading: ElLoading,
