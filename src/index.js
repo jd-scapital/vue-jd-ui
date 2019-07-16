7584dbc3e57d342d02218eb481e9a01d19a24906
siqing <siqing@scapital.com>
2019-07-16 17:43:50 +0800

    [build] 1.0.0-beta.34

--- src/index.js
+++ src/index.js
@@ -88,7 +88,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '1.0.0-beta.33',
+  version: '1.0.0-beta.34',
   install,
   CollapseTransition,
   Loading: ElLoading,
