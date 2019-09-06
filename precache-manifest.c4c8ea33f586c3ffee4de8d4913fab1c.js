bd49e07b1fa3efb497d759bb37892619379a63df
roastwind <muzi131313@gmail.com>
2019-09-06 15:21:01 +0800

    Updates

---
+++ precache-manifest.c4c8ea33f586c3ffee4de8d4913fab1c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfdb67e96e42e6d42b6271a34e5f43c0",
+    "url": "/vue-jd-ui/fonts/vue-jd-ui-ie.bfdb67e9.eot"
+  },
+  {
+    "revision": "d3e973953446d0c94b8f",
+    "url": "/vue-jd-ui/js/about.3a09708f.js"
+  },
+  {
+    "revision": "6038436556a7bf4ebf7d",
+    "url": "/vue-jd-ui/js/chunk-vendors.f6454c4e.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-jd-ui/robots.txt"
+  },
+  {
+    "revision": "762a41d16ab24f01a89b",
+    "url": "/vue-jd-ui/js/app.6f7beb29.js"
+  },
+  {
+    "revision": "8edbf0b2ad28f0c4409599215630643f",
+    "url": "/vue-jd-ui/index.html"
+  },
+  {
+    "revision": "b112211d4c7dd32a775b3ebe908089ab",
+    "url": "/vue-jd-ui/fonts/vue-jd-ui.b112211d.ttf"
+  },
+  {
+    "revision": "047c4abeb28f65b857eca07e4fa6939d",
+    "url": "/vue-jd-ui/fonts/vue-jd-ui.047c4abe.woff"
+  },
+  {
+    "revision": "bfdb67e96e42e6d42b6271a34e5f43c0",
+    "url": "/vue-jd-ui/fonts/vue-jd-ui-ie-old.bfdb67e9.eot"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/vue-jd-ui/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/vue-jd-ui/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "9078d942e56acec0d4097e2a7ee5e336",
+    "url": "/vue-jd-ui/css/bootstrap.min.css"
+  }
+];
