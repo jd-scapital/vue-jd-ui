42f12004c91a3d8182a71029a788c718597b538c
roastwind <muzi131313@gmail.com>
2019-09-18 17:21:52 +0800

    [build] 1.0.0-beta.41

--- src/index.js
+++ src/index.js
@@ -88,7 +88,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '1.0.0-beta.40',
+  version: '1.0.0-beta.41',
   install,
   CollapseTransition,
   Loading: ElLoading,
